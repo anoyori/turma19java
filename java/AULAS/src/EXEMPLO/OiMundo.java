1c5bdbf2c988f5876ef65d0669f849c5b7eabe27
Amanda <anoyori@gmail.com>
2021-03-24 16:45:12 -0300

    Aula 1 - JAVA

---
+++ java/AULAS/src/EXEMPLO/OiMundo.java
@@ -0,0 +1,12 @@
+package EXEMPLO;
+
+public class OiMundo {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("Oi turma 19 generation");
+	System.out.println("Vamos programar em Java!");
+	
+	
+	}
+}

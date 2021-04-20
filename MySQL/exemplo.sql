0674eafd32a5150e53137b1167bcf30f8a614b93
Amanda <anoyori@gmail.com>
2021-04-20 14:15:29 -0300

    Exercícios de MySQL

---
+++ MySQL/exemplo.sql
@@ -0,0 +1,5 @@
+insert into tb_produtos (nome, preco, marca_id) values ("Tênis", 99.99, 3);
+insert into tb_produtos (nome, preco, marca_id) values ("Saia", 179.99, 5);
+insert into tb_produtos (nome, preco, marca_id) values ("Calça", 129.99, 8);
+
+select * from tb_produtos
